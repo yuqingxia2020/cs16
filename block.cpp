6964fe77a3bf111bd3375a923c673d1ed90a144c
Yuqing Xia <yuqingxia@csil-02.cs.ucsb.edu>
2020-01-14 16:06:30 -0800

    First Commit

---
+++ block.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int mian() {
+	int row, column, count;
+	cout<< "Enter number of rows and colums:"<<endl;
+	cin >> line >> column;
+	for( int ; 
+
